36b1a82812c5540355c5c634df227e1cf807b304
Pawan Bonde <pssbonde@gmail.com>
2025-07-31 17:47:04 +0530

    Updating requirments.txt file again.

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # set up the base image
-FROM python:3.10
+FROM python:3.11
 
 # set the working directory
 WORKDIR /app/
--- data_cleaning.py
+++ data_cleaning.py
@@ -2,7 +2,7 @@ import pandas as pd
 import os
 
 # ✅ Set the working directory (change this path as needed)
-os.chdir("D:\Project")
+os.chdir("/app")
 
 # ✅ Define the data path
 DATA_PATH = "music_info.csv"
